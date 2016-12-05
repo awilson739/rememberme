5422503c818bef2c5b7b035a272138886938ba57
Angela Wilson <angela_wilson@sra.com>
2016-12-05 09:59:17 -0500

    Edited db model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 class User(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    nickname = db.Column(db.String(64),index=True,unique=True)
+   password = db.Column(db.String(12),unique=True)
    email = db.Column(db.String(120),index=True,unique=True)
    task = db.relationship('Task',lazy='dynamic')
    @property 
